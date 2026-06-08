b687d60422a275c322b457023db824c47b6ecd9b
xmaxinex4 <maxinegerhard@gmail.com>
2026-06-08 18:18:06 -0400

    Fixing mobile scrolling to top

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import smoothscroll from "smoothscroll-polyfill";
+smoothscroll.polyfill();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
